84ae6ad21039129665a931f04403c9d77aec2f91
hellofaye <272347895@qq.com>
2021-09-17 23:10:53 +0800

    [say hello]

--- websocket/src/main/java/com/faye/websocket/WebsocketApplication.java
+++ websocket/src/main/java/com/faye/websocket/WebsocketApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebsocketApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebsocketApplication.class, args);
     }
