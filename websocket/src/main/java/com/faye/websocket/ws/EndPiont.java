7a1cfca769a78d89e3bec98226c81b7169d3be5b
hellofaye <272347895@qq.com>
2021-09-17 23:56:09 +0800

    [websocket init] ]

---
+++ websocket/src/main/java/com/faye/websocket/ws/EndPiont.java
@@ -0,0 +1,39 @@
+package com.faye.websocket.ws;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ServerEndpoint(value = "/chat")
+public class EndPiont {
+
+    /*
+     * 一个ChatEndPiont 表示一个WebSocket 连接
+     * 存储使用用户的webSocket  static 表示不属于任何一个对象，才能存储所有的 “用户”
+     * 使用线程安全的容器
+     */
+    private static Map<String, EndPiont> onLineUsers = new ConcurrentHashMap<>();
+
+    //声明一个session对象，通过该对象可以发送消息给指定用户，不能设置为静态，每个ChatEndPoint有一个session才能区分.(websocket的session)
+    private Session session;
+
+    //建立链接时 触发
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig config){
+
+    }
+
+    //服务器接受到消息 触发
+    @OnMessage
+    public void onMessage(String message,Session session){
+
+    }
+
+    //用户断开链接时 触发
+    @OnClose
+    public void onClose(Session session){
+
+    }
+
+}
