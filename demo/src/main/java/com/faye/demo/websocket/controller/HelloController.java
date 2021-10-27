c1b5bab7f508b3b026d4e2f0aff2e554a8971173
cyf <chenyunfei@inman.cc>
2021-10-27 21:59:23 +0800

    rocketmq

--- demo/src/main/java/com/faye/demo/websocket/controller/HelloController.java
+++ demo/src/main/java/com/faye/demo/websocket/controller/HelloController.java
@@ -1,13 +1,13 @@
 package com.faye.demo.websocket.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-    @GetMapping("/helloWorld")
-    public String hello(){
-        return "HelloWorld!!!";
-    }
-}
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class HelloController {
+//
+//    @GetMapping("/helloWorld")
+//    public String hello(){
+//        return "HelloWorld!!!";
+//    }
+//}
--- demo/src/test/java/com/faye/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/faye/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.faye.demo;
 
-import org.junit.jupiter.api.Test;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class DemoApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTests {
+
+    @Autowired
+    private RocketMQTemplate rocketMQTemplate;
 
     @Test
-    void contextLoads() {
+    public void testSendMessage(){
+        rocketMQTemplate.convertAndSend("springboot-rocketmq","hello springboot rocketmq");
     }
 
 }
