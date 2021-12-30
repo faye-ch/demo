78f00e0ff3f8b7a4b9bda9849a0193672a40aae3
cyf <chenyunfei@inman.cc>
2021-12-30 10:36:40 +0800

    [12/30]BlockQueue 实现生产者消费者的问题

--- demo/src/main/java/com/faye/demo/ex/ProducerOnComsumer.java
+++ demo/src/main/java/com/faye/demo/ex/ProducerOnComsumer.java
@@ -5,6 +5,7 @@ import java.util.concurrent.BlockingQueue;
 
 /*
  * @Author cyf
+ * BlockingQueue 实现生产者消费者问题
  * 提供了阻塞的 take() 和 put() 方法：如果队列为空 take() 将阻塞，直到队列中有内容；如果队列为满 put() 将阻塞，直到队列有空闲位置。
  * @Date 2021/12/30
  **/
