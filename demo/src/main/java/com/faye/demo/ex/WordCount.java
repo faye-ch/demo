898f4b515074983160478882b8849c56621e11b7
cyf <chenyunfei@inman.cc>
2022-01-06 11:33:28 +0800

    [1/6]多线程词频分析

--- demo/src/main/java/com/faye/demo/ex/WordCount.java
+++ demo/src/main/java/com/faye/demo/ex/WordCount.java
@@ -3,11 +3,16 @@ package com.faye.demo.ex;
 import org.junit.Test;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 
 /*
  * @Author cyf
@@ -16,6 +21,9 @@ import java.util.StringTokenizer;
  **/
 public class WordCount {
 
+    final ForkJoinPool pool = ForkJoinPool.commonPool();
+
+    //单线程
     @Test
     public void compare_with_single()throws Exception{
         String path = "C:\\Users\\Administrator.DESKTOP-EFOTAD8\\Desktop\\text.txt";
@@ -37,8 +45,8 @@ public class WordCount {
             }
         }
 
-        System.out.println(totalMap.get("ababb"));
-        System.out.println(totalMap.size());
+        System.out.println("cnt:"+totalMap.get("ababb"));
+        System.out.println("size:"+totalMap.size());
         System.out.println(System.currentTimeMillis()-start);
 
     }
@@ -60,5 +68,77 @@ public class WordCount {
             map.put(s,n);
         }
     }
+
+    //实现线程的三种方式：继承 Thread、实现Runable、实现Callable
+    //Callable 可以返回结果
+    class CountTask implements Callable{
+
+        private final long start;
+        private final long end;
+        private final String filename;
+
+        public CountTask(String filename,long start,long end){
+            this.start = start;
+            this.end = end;
+            this.filename = filename;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            //使用 RandomAccessFile 读取流数据
+            FileChannel channel = new RandomAccessFile(filename, "rw").getChannel();
+            //将流数据映射到用户内存空间
+            // 内存映射文件
+            // [start, end] -> Memory
+            // Device -> Kernel Space -> UserSpace(buffer) -> Thread
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, start, end - start);
+            //解码 解析为字符串
+            String str = StandardCharsets.US_ASCII.decode(buffer).toString();
+            return countByString(str);
+        }
+    }
+
+    public void run(String filename,long chunkSize)throws Exception{
+        File file = new File(filename);
+        long fileSize = file.length();
+        List<ForkJoinTask<Map<String, Integer>>> list = new ArrayList<>();
+        long start = System.currentTimeMillis();
+        //读取起始位置
+        long position = 0;
+        while (position<fileSize){
+            long next = Math.min(position + chunkSize, fileSize);
+            CountTask task = new CountTask(filename, position, next);
+            position = next;
+            ForkJoinTask<Map<String, Integer>> future = pool.submit(task);
+            list.add(future);
+        }
+
+        Map<String, Integer> totalMap = new HashMap<>();
+        for (ForkJoinTask<Map<String, Integer>> future : list) {
+            Map<String, Integer> map = future.get();
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                incKey(entry.getKey(),totalMap,entry.getValue());
+            }
+        }
+
+        System.out.println("cnt:"+totalMap.get("ababb"));
+        System.out.println("size:"+totalMap.size());
+        System.out.println(System.currentTimeMillis()-start);
+    }
+
+    @Test
+    public void count()throws Exception{
+        String path = "C:\\Users\\Administrator.DESKTOP-EFOTAD8\\Desktop\\text.txt";
+        WordCount count = new WordCount();
+        count.run(path,1024*128);
+
+        // 阿姆达定律
+        // 120s(单线程) -> 16core(16核多线程) -> 120/16 = ?
+        // P(不可多线程执行的任务)    NP(可多线程执行的任务)
+        // 词频分析是可多线程执行的任务，但是从磁盘读取数据并不是可多线程执行的任务
+        // 所以速度不能解决理论值
+    }
+
+
 
 }
