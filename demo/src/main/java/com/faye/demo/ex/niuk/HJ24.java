fa2ca348052decd32695e10d053cb69eea2abdc9
cyf <chenyunfei@inman.cc>
2022-05-23 10:40:57 +0800

    Leetcode376

--- demo/src/main/java/com/faye/demo/ex/niuk/HJ24.java
+++ demo/src/main/java/com/faye/demo/ex/niuk/HJ24.java
@@ -18,7 +18,17 @@ public class HJ24 {
             //计算每个位置左侧的最长递增
             //nums[i] > nums[j] : dp[i] = max(dp[i],dp[j]+1)
             //dp[i] 上一次的累加最大值
-            //dp[j]+1,nums[i] > nums[j]说明nums[i]比dp[j]上累计的数都要大 因此在dp[j]+1
+            //dp[j]+1,nums[i] > nums[j]说明nums[i]比dp[j]上累计的数都要大 因此在dp[j]+1 如下面例子3
+            //为什么要用 dp[i] 于 dp[j]+1 做比较，有dp[j]累计的值比前面少的情况,如下面例子5
+
+            //1 2 3 3 2 12
+            //1.dp[0] = 0
+            //2.dp[1] = i=1 j=0:2 1 dp[1]=1
+            //3.dp[2] = 1=2 j=0:3 1 dp[1]=1 ,i=2 j=1:3 dp[1]=2 ,
+            // dp[1]累计的值为：1、2, nums[i]=3 > nums[j]=2 就必然大于dp[1]中累计的值，所以可以继承dp[1]的值，在其后面追加 +1
+            //4.dp[3] = 2
+            //5.dp[4] = 1
+            //6.dp[5] = 3  这就是比较dp[i]的用处了，dp[i] 在 j=4 时 dp[i] = max(dp[5],dp[3+1]) = 3 若dp[j]+1 = 2
             for (int i = 0; i < n; i++) {
                 left[i] = 1;
                 for (int j = 0; j < i; j++) {
